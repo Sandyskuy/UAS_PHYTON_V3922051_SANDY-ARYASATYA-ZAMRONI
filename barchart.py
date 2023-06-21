4a3bed6164e1a9f780a72f6ff4f98f18d1ef94e4
Sandyskuy <127075058+Sandyskuy@users.noreply.github.com>
2023-06-21 15:29:24 +0700

    Add files via upload

---
+++ barchart.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import library yang diperlukan
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Membaca database
+data = pd.read_csv("C:/sandy/522466531_bukalapak.csv")
+
+# Membuat line chart
+plt.bar(data['jumlah_sold'], data['jumlah_rating'])
+
+# Menambahkan judul ke plot
+plt.title("Jumlah Rating dan Jumlah Sold")
+
+# Menetapkan label sumbu X dan Y
+plt.xlabel('Jumlah Sold')
+plt.ylabel('Jumlah Rating')
+
+# Menampilkan plot
+plt.show()
+
+
+# In[ ]:
+
+
+
+
